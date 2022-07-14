faf522272f4d8b942aa0f90d4430d612590f9caf
TanmayJain33 <tanmay.jain@crownstack.com>
2022-07-14 17:36:48 +0530

    Refractor Snake's Head

--- src/components/Head/head.tsx
+++ src/components/Head/head.tsx
@@ -1,17 +1,30 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 
 export default function Head({position, size}: any) {
   return (
-    <View
-      style={{
-        width: size,
-        height: size,
-        backgroundColor: '#f00',
-        position: 'absolute',
-        left: position[0] * size,
-        top: position[1] * size,
-      }}
-    />
+    <View style={styles(size, position).headStyle}>
+      <View style={styles(size, position).pearlStyle} />
+    </View>
   );
 }
+
+const styles = (size: number, position: number[]) =>
+  StyleSheet.create({
+    headStyle: {
+      width: size,
+      height: size,
+      backgroundColor: '#006400',
+      position: 'absolute',
+      left: position[0] * size,
+      top: position[1] * size,
+    },
+    pearlStyle: {
+      marginLeft: 5,
+      marginTop: 5,
+      borderRadius: size / 2,
+      width: size / 2,
+      height: size / 2,
+      backgroundColor: '#ffd700',
+    },
+  });
