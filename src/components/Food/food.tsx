233e7bc2a720405e8dced19c49fabc03f233bc4f
TanmayJain33 <tanmay.jain@crownstack.com>
2022-07-14 17:55:37 +0530

    Refractor food component

--- src/components/Food/food.tsx
+++ src/components/Food/food.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet, Image} from 'react-native';
 
 export default function Food({position, size}: any) {
   return (
-    <View
-      style={{
-        width: size,
-        height: size,
-        backgroundColor: '#0f0',
-        position: 'absolute',
-        left: position[0] * size,
-        top: position[1] * size,
-        borderRadius: 50,
-      }}
-    />
+    <View>
+      <Image
+        style={styles(size, position).foodStyle}
+        source={require('../../assets/images/apple.png')}
+      />
+    </View>
   );
 }
+
+const styles = (size: number, position: number[]) =>
+  StyleSheet.create({
+    foodStyle: {
+      width: size,
+      height: size,
+      position: 'absolute',
+      left: position[0] * size,
+      top: position[1] * size,
+    },
+  });
